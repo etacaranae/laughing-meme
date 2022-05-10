391f2a66385bcc5fdab03ac34264ffdc96f02522
s-ALEAVITT <s-ALEAVITT@484E293863.lwsd.k12wa.org>
2022-05-10 13:45:05 -0700

    wrote a small message

---
+++ src/HelloGit.java
@@ -0,0 +1,7 @@
+
+public class HelloGit {
+	public static void main(String[] args) {
+		System.out.println("Hello Github! \nThis is in a git repo, right? \nI sure hope so!");
+		System.out.println("Hello Github! \nThis is in a git repo, right? \nI sure hope so!");
+	}
+}
